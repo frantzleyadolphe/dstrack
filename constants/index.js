3dc8d5821237b0f1f8f5840edc5b27883c5da9b0
Adolphe Frantzley <83029225+frantzleyadolphe@users.noreply.github.com>
2025-06-26 23:50:07 -0400

    constant file created

---
+++ constants/index.js
@@ -0,0 +1,29 @@
+import { Dimensions } from "react-native";
+
+export const SCREENSIZE = {
+  deviceHeight: Dimensions.get("window").height,
+  deviceWidth: Dimensions.get("window").width,
+};
+
+export const COLORS = {
+  primary: "#0D40FF",
+  secondary: "#F2F2F2",
+  white: "#FFFFFF",
+  black: "#000000",
+};
+
+export const MARGIN = {
+  horizontal: 16,
+  vertical: 16,
+};
+
+// export const SIZE={
+//     title:22,
+//     susbtitle:14
+// };
+
+// export const FONT = {
+//   SfProBold: "SfProBold",
+//   SfProRegular: "SfProRegular",
+//   SfProMedium: "SfProMedium",
+// };
